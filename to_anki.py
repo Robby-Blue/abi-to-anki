d83c0adae380536bf2adf78d523125a8959203a0
Robby-Blue <69482496+Robby-Blue@users.noreply.github.com>
2025-08-09 16:31:09 +0200

    Initial Commit

---
+++ to_anki.py
@@ -0,0 +1,39 @@
+import hashlib
+import json
+import os
+
+def gen_uid(question):
+    text = question["title"] + "---" + question["solution"]
+    return hashlib.md5(text.encode("UTF8")).hexdigest()
+
+def recurse(path):
+    data = []
+
+    for name in os.listdir(path):
+        sub_path = os.path.join(path, name)
+        if os.path.isdir(sub_path):
+            data += recurse(sub_path)
+        else:
+            data += read_file(sub_path)
+    
+    return data
+
+def read_file(path):
+    with open(path, "r") as f:
+        data = json.load(f)
+    
+    generated = False
+    for question in data:
+        if "guid" not in question:
+            question["guid"] = gen_uid(question)
+            generated = True
+    
+    if generated:
+        with open(path, "w") as f:
+            json.dump(data, f, indent=2)
+
+    return data
+
+data = recurse("subjects")
+print(len(data))
+# TODO: make anki deck out of data
